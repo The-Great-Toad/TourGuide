f66f40941e251d80e39bb8a396f1e464e2507dc8
The-Great-Toad <bearcode26@gmail.com>
2025-05-09 09:55:06 +0200

    Fix - Surefire parsing error & TestPerformance.highVolumeGetRewards
    - Surefire error: Failed to parse time value: For input string: "1,616.719"
    - add minimum 50 threads ForkJoinPool's pool size for CI pipeline performance

--- src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
+++ src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
@@ -6,6 +6,8 @@ import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
 import gpsUtil.location.Location;
 import gpsUtil.location.VisitedLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import rewardCentral.RewardCentral;
 
@@ -14,12 +16,14 @@ import java.util.concurrent.ForkJoinPool;
 
 @Service
 public class RewardsService {
+	private static final Logger log = LoggerFactory.getLogger(RewardsService.class);
     private static final double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
 
 	// proximity in miles
-    private final int defaultProximityBuffer = 10;
-	private int proximityBuffer = defaultProximityBuffer;
-	private final int attractionProximityRange = 200;
+    private static final int defaultProximityBuffer = 10;
+	private static int proximityBuffer = defaultProximityBuffer;
+	private static final int attractionProximityRange = 200;
+
 	private final GpsUtil gpsUtil;
 	private final RewardCentral rewardsCentral;
 	private final ForkJoinPool forkJoinPool;
@@ -29,12 +33,15 @@ public class RewardsService {
 		this.rewardsCentral = rewardCentral;
 
 		int processors = Runtime.getRuntime().availableProcessors();
-		int poolSize = processors * 10;
+//		int poolSize = processors * 10;
+		// Minimum pool size of 50 threads (for CICD)
+		int poolSize = Math.max(50, processors * 10);
+		log.info("Available processors: {}", processors);
 		forkJoinPool = new ForkJoinPool(poolSize);
 	}
 	
 	public void setProximityBuffer(int proximityBuffer) {
-		this.proximityBuffer = proximityBuffer;
+		RewardsService.proximityBuffer = proximityBuffer;
 	}
 	
 	public void calculateRewards(User user) {
@@ -80,8 +87,7 @@ public class RewardsService {
                                + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
 
         double nauticalMiles = 60 * Math.toDegrees(angle);
-        double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
-        return statuteMiles;
+        return STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
 	}
 
 }
